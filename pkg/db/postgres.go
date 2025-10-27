928d4ad24759e18b9c31758a749da6cf2820922a
Mandar Yadav <yadavm2324@gmail.com>
2025-10-27 17:00:18 +0000

    Added pkg directory with dbconfig.go and postgres.go files

---
+++ pkg/db/postgres.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"airline-booking/pkg/config"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+var DB *sqlx.DB
+
+// ConnectPostgres initializes the DB connection pool
+func ConnectPostgres(cfg *config.PostgresConfig) (*sqlx.DB, error) {
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode)
+
+	db, err := sqlx.Connect("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("Error connecting to postgres: %w", err)
+	}
+
+	// Connection pool settings
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(time.Hour)
+
+	// Check the connection
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("Error pinging postgres: %w", err)
+	}
+
+	log.Println("Connected to Postgres successfully")
+	return db, nil
+
+}
